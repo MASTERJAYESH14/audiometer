d47507c5c6968d12384e98d9670d28337ba6e21b
VALLABH S D <145659908+vallabhsd@users.noreply.github.com>
2023-09-22 16:01:02 +0530

    Add files via upload

---
+++ graphs.py
@@ -0,0 +1,28 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.floatlayout import FloatLayout
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+import matplotlib.pyplot as plt
+# Define what we want to graph X = []]
+x = [0,250,500,1000,2000,4000,8000]
+y = [-10,-5,0,5,10,15,20,25,30]
+
+plt.plot(x,y)
+plt.ylabel("Frequencies")
+plt.xlabel("Decibals")
+
+class Graph(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        box = self.ids.box
+        box.add_widgets(FigureCanvaskivyAgg(plt.gcf()))
+
+    def save_it(self):
+        pass
+class MainApp (MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return Builder.load_file('matty.kv')
+MainApp().run()
